4a1a889dd60f08a3d6e0498cffdc7fc5cc920996
Xurshid <xurshidtrading06@gmail.com>
2025-04-23 07:00:10 +0500

    make limit smaller

--- service/getCategories.ts
+++ service/getCategories.ts
@@ -5,7 +5,7 @@ import { useQuery } from "@tanstack/react-query"
 export const getCategories = () => {
     const {data = [], isLoading, isError} = useQuery(({
         queryKey: ['categories'],
-        queryFn: () => instance().get("/categories/all", {params:{limit:1000}}).then(res => res.data)
+        queryFn: () => instance().get("/categories/all", {params:{limit:10}}).then(res => res.data)
     }))
 
     return{data, isLoading, isError}
